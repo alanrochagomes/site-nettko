51d9c94d66184b6f27b07b3ad08f995acc130dbd
alanrochagomes <alanrochaarg2001@gmail.com>
2024-09-08 14:55:32 -0300

    add: novas funcionalidades

--- scripts/nav.js
+++
@@ -1,9 +0,0 @@
-const nav = document.getElementsByTagName("nav")[0];
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 10) {
-    nav.classList.add("nav-dark");
-  } else {
-    nav.classList.remove("nav-dark");
-  }
-});
